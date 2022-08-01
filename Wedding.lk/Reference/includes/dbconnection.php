3c325e821b3d68916e50e9fea19f91e6af4b1c63
UdeepaSandakal <81462130+UdeepaSandakal@users.noreply.github.com>
2022-08-01 20:39:59 +0530

    2022/01/8 @8.39PM

--- Wedding.lk/Reference/includes/dbconnection.php
+++ Wedding.lk/Reference/includes/dbconnection.php
@@ -1,14 +1,12 @@
 <?php
-	$server="localhost";
-	$username="root";
-	$password="";
-	$db="wedding";
+	//$connection=mysqli_connect(dbserver,dbuser,dbpassword,dbname);
+	$connection=mysqli_connect('localhost','root','','wedding');
 
-	$conn=mysqli_connect($server,$username,$password,$db);
-
-	if($conn){
-		echo "Successfully connected with database";
+	//mysqli_connect_errno(); mysqli_connect_error()
+	//checking the connection
+	if(mysqli_connect_error()){
+		die('Database connection failed'.mysqli_connect_error());
 	}else{
-		echo "Not connected to database";
+		echo "Connection successfull";
 	}
 ?>
